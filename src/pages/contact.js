6319cba08356eb1b738af57b4532d9d023c39eb5
Adnan Reza <adnan.reza@outlook.com>
2020-06-05 10:48:20 -0700

    Added footer and header components and rendered them on pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,17 @@
 import React from "react"
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const Contact = () => {
   return (
     <div>
+      <Header />
       <h1>Contact</h1>
       <p>You can reach me at my email addres</p>
       <a href="https://twitter.com/aRazr47/" target="_blank">
         My Twitter Handle
       </a>
+      <Footer />
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello</h1>
       <h2>I'm Adnan, a web development instructor, living in Vancouver, BC.</h2>
       <p>
         Need a developer? <Link to="/contact">Contact Me</Link>
       </p>
+      <Footer></Footer>
     </div>
   )
 }

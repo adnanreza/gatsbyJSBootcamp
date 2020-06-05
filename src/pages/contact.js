2ab5c7b7b4e539d925c4690cc87016e25cff2e09
Adnan Reza <adnan.reza@outlook.com>
2020-06-05 11:04:26 -0700

    Installed gatsby scss plugin

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <div>
         <h1>Contact</h1>
         <p>You can reach me at my email addres</p>
-        <a href="https://twitter.com/aRazr47/" target="_blank">
+        <a href="https://twitter.com/aRazr47/" target="_blank" rel="noreferrer">
           My Twitter Handle
         </a>
       </div>
